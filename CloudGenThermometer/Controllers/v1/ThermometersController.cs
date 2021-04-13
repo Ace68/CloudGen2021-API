156a1256dfd7e49a582668198e341abc65b1898d
Alberto <alberto.acerbis@4solid.it>
2021-04-13 18:12:21 +0200

    Add Controller

---
+++ CloudGenThermometer/Controllers/v1/ThermometersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CloudGenThermometer.Shared.Abstracts;
+using CloudGenThermometer.Shared.CustomTypes;
+using CloudGenThermometer.Shared.JsonModel;
+using CloudGenThermometer.Shared.Services;
+using FourSolid.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudGenThermometer.Controllers.v1
+{
+    public class ThermometersController : BaseController
+    {
+        private readonly IThermometerServices _thermometerServices;
+
+        public ThermometersController(ILoggerFactory loggerFactory,
+            IThermometerServices thermometerServices) : base(loggerFactory)
+        {
+            this._thermometerServices = thermometerServices;
+        }
+
+        [HttpGet]
+        [Route("devices/{deviceId}")]
+        public async Task<ActionResult<IEnumerable<ThermometerTrendJson>>> GetThermometerTrend([FromRoute] string deviceId)
+        {
+            try
+            {
+                var trend = await this._thermometerServices.GetThermometerTrendAsync(new DeviceId(deviceId.ToGuid()));
+                return new ActionResult<IEnumerable<ThermometerTrendJson>>(trend);
+            }
+            catch (Exception ex)
+            {
+                this.Logger.LogError(CommonServices.GetDefaultErrorTrace(ex));
+                return this.BadRequest(CommonServices.GetErrorMessage(ex));
+            }
+        }
+    }
+}
